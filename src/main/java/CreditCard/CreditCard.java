585bc3c59af0674a0b14647fc03580cd8ed30947
TootsyRolls <tonygian812@gmail.com>
2024-12-01 22:49:30 -0500

    Updated 29/11/24

---
+++ src/main/java/CreditCard/CreditCard.java
@@ -0,0 +1,50 @@
+package CreditCard;
+
+import Classes.*;
+
+public class CreditCard{
+    private Person owner;
+    private Money balance;
+    private Money creditLimit;
+
+    // Create new credit card account
+    public CreditCard(Person owner, Money balance) {
+        this.owner = owner;
+        this.balance = new Money(0);
+        this.creditLimit = new Money(creditLimit); // Make sure it's the same as set above
+    }
+
+    // Accessors
+    public String getPersonals(){
+        return owner.toString();
+    }
+
+    public Money getBalance(){
+        return new Money(balance);
+    }
+
+    public Money getCreditLimit(){
+        return new Money(creditLimit);
+    }
+
+    // Charge Method
+    public void charge(Money amount){
+        Money newBalance = new Money(amount);
+
+        if (newBalance.compareTo(creditLimit) <= 0){
+            balance = newBalance;
+            System.out.println("Charge: " + amount);
+
+        }
+        else{
+            System.out.println("Your new balance exceeds the credit limit");
+        }
+    }
+
+    // Payment Method
+    public void payment(Money amount){
+        balance = balance.subtract(amount);
+        System.out.println("Payment: " + amount);
+    }
+}
+//revision 30/11/24
